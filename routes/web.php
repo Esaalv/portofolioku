01b826495ce74a1ad8f3ec7a77305729c9dd0392
esaalv <esacanoealviank@gmail.com>
2026-02-21 04:12:59 +0700

    ##

--- routes/web.php
+++ routes/web.php
@@ -119,9 +119,3 @@ Route::get('/clear-semua', function() {
     ";
 });
 
-Route::get('/ganti-pass-rahasia', function() {
-    $user = \App\Models\User::find(1);
-    $user->password = Hash::make('Esa@2307089');
-    $user->save();
-    return "Password admin berhasil diganti!";
-});
